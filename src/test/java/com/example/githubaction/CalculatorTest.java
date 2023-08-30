7576ba46aa4e4848dd6fc34f8b024b30eb57ab9b
heenahan <mby1997s@gmail.com>
2023-08-30 13:55:49 +0900

    test: 실패 테스트

--- src/test/java/com/example/githubaction/CalculatorTest.java
+++ src/test/java/com/example/githubaction/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.example.githubaction;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -9,9 +10,17 @@ class CalculatorTest {
     Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("2 + 3 = 5")
     void test() {
         int result = calculator.add(2, 3);
         assertThat(result).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("실패하는 테스트")
+    void failTest() {
+        int result = calculator.add(2, 3);
+        assertThat(result).isEqualTo(1);
     }
 
 }
